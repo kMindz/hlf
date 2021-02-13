eaeb755e7fb0121a28832f6153fa4d3ed7814b93
Mustafa <36147068+mustafa-kmindz@users.noreply.github.com>
2021-02-13 20:30:04 +0530

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -2,3 +2,9 @@ sudo adduser kmindz --gecos "M H,,," --disabled-password
 echo "kmindz:kmindz321" | sudo chpasswd
 
 usermod -aG sudo  kmindz
+
+mv ./prereqs-ubuntu.sh /home/kmindz/
+
+cd /home/kmindz/
+
+su kmindz
